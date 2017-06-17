8ec19b092776cce5f73714373cc8dc38597f249f
viscropst <bcct2010@live.cn>
2017-06-17 20:50:13 +0800

    My tweak and AEX support

---
+++ aosp_libra.mk
@@ -0,0 +1,37 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit 64-bit configs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/libra/device.mk)
+
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := aosp_libra
+PRODUCT_DEVICE := libra
+
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi-4c
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+     BUILD_FINGERPRINT="Xiaomi/libra/libra:7.0/NRD90M/V8.2.3.0.NXKCNEC:user/release-keys" \
+    PRIVATE_BUILD_DESC="libra-user 7.0 NRD90M V8.2.3.0.NXKCNEC release-keys"
+
+
+TARGET_OTA_ASSERT_DEVICE := libra
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Build Magisk
+DEFAULT_ROOT_METHOD := magisk   
