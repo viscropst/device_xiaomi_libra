b4704214a3a42fdd1f3d9d54beec1100c5bcd572
viscropst <bcct2010@live.cn>
2017-06-21 09:48:34 +0800

    Update qcom-platform.mk

--- board/qcom-platform.mk
+++ board/qcom-platform.mk
@@ -11,13 +11,3 @@ TARGET_PER_MGR_ENABLED := true
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
-#TARGET_USE_SDCLANG := true
-ifneq ($(HOST_OS),darwin)
-
-SDCLANG := true
-
-SDCLANG_PATH := prebuilts/snapdragon/llvm-3.8/bin
-
-SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
-
-endif
