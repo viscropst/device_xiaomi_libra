fcf7c1ec29f7f3d433454172574c7ec43de771c9
viscropst <bcct2010@live.cn>
2017-08-07 11:54:25 +0800

    Let's use the sdclang by vendor

--- board/sdclang.mk
+++ board/sdclang.mk
@@ -1,11 +1,5 @@
-#ifneq ($(HOST_OS),darwin)
+ifneq ($(HOST_OS),darwin)
+SDCLANG = true
+SDCLANG_FORCED = true
 
-#SDCLANG := true
-
-#SDCLANG_PATH := prebuilts/snapdragon/llvm-3.8/bin
-
-#SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
-#SDCLANG_LTO_DEFS := device/qcom/common/sdclang/sdllvm-lto-defs.mk
-
-
-#endif
+endif
