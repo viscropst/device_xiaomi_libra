e1e685200eab463398d16b9a4eb33ad6e4be3150
viscropst <bcct2010@live.cn>
2017-08-18 23:08:47 +0800

    Add some permissions to support NOISE

---
+++ product/permission.mk
@@ -0,0 +1,7 @@
+PRODUCT_COPY_FILES += \
+      frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+      frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+      frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+      frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+      frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.faketouch.xml \
+      frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
