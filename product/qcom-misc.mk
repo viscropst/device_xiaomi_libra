0f110373d85325b2a4c7ca85b1eda6583f6526a2
Demon000 <demonsingur@gmail.com>
2017-08-23 21:03:43 +0300

    product: build vibrator hal
    Change-Id: I3102d5de464bf89719673e8233e857da1dea3ef9

--- product/qcom-misc.mk
+++ product/qcom-misc.mk
@@ -18,3 +18,7 @@ PRODUCT_PACKAGES += \
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
