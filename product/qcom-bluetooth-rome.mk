e38983a8d0c920c5ef34e7cf915ef19ffafbd5bc
Demon000 <demonsingur@gmail.com>
2017-08-23 21:15:47 +0300

    product: build bluetooth hal
    Change-Id: I7f04f77addfe43a27e2fbd720c05f4b8fd936a3c

--- product/qcom-bluetooth-rome.mk
+++ product/qcom-bluetooth-rome.mk
@@ -1,3 +1,7 @@
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml
