30a9e5c1ee7f71d1d9be681e19f7ac99a999d130
Demon000 <demonsingur@gmail.com>
2017-08-23 21:02:43 +0300

    product: add gatekeeper hal;
    Change-Id: I2f774168380faa84106270c42ba9af78420dcc18

--- product/qcom-misc.mk
+++ product/qcom-misc.mk
@@ -14,3 +14,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Keymaster HAL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
+
+# Gatekeeper HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
