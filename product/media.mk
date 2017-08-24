b34c2004cdae26b460f69b5e093e3d9448446a0a
Demon000 <demonsingur@gmail.com>
2017-08-24 21:02:01 +0300

    use aosp media configs
    Change-Id: I5ae4eb66720e85c81c1436fd1992a73e00c8ab42

--- product/media.mk
+++ product/media.mk
@@ -6,3 +6,4 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs_performance.xml
