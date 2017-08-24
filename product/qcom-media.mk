87bbf741fd7f11b95f1f55b2796f73067064a6df
Demon000 <demonsingur@gmail.com>
2017-08-24 06:48:02 +0300

    product: update media for O
    Change-Id: Id3a4e43a13f1215a9f088d093bc24f74644b56be

--- product/qcom-media.mk
+++ product/qcom-media.mk
@@ -1,15 +1,10 @@
 # Media
+
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
     libc2dcolorconvert \
-    libdivxdrmdecrypt \
-    libOmxAacEnc \
-    libOmxAmrEnc \
+    libstagefrighthw \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxQcelp13Enc \
     libmm-omxcore \
     libOmxVdec \
     libOmxVdecHevc \
-    libOmxVenc \
-    libstagefrighthw
+    libOmxVenc
