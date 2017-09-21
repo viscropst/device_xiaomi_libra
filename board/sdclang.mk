0cc8f2dac8b04a03e726f81794d9ac53bc307d90
viscropst <bcct2010@live.cn>
2017-09-21 18:37:01 +0800

    Change clang for AEX

--- board/sdclang.mk
+++ board/sdclang.mk
@@ -1,9 +1,7 @@
 ifneq ($(HOST_OS),darwin)
 
 SDCLANG := true
-SDCLANG_PATH := prebuilts/snapdragon/llvm-3.8/bin
-
-SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
+include vendor/aosp/sdclang/sdclang.mk
 
 endif
 
